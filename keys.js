78a2a93689e92fd8ab3b445d3dd8bf7243de39aa
thomromano <thomromano@gmail.com>
2018-04-21 22:14:18 -0500

    add twitter and spotify keys

--- keys.js
+++ keys.js
@@ -1,13 +1,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TuBHADXPCZn8eYhnDE7TkxVV3,
+  consumer_secret: process.env.e5CgjplghkRzL4WdulchO3fwwNPQusGqkhq9tWkAnscxhYSEYS,
+  access_token_key: process.env,987875336661676032:yMh2Bzy5V0re6bc7yBacCn9uiZUug3X,
+  access_token_secret: process.env.dczROM2am4uq1qTKrmaLtP92pTV8Qg5btHDDrAywSfZlW
+  
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.bed5d363080c48b293312a01433c2188,
+  secret: process.env.a241546e79b8452eb10ef972bd5533bb
 };
