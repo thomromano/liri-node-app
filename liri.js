b0f0b6fc24b65d8d26a5b9c71f4848894f1d2816
thomromano <thomromano@gmail.com>
2018-04-24 15:04:40 -0500

    fix

--- liri.js
+++ liri.js
@@ -13,36 +13,36 @@ console.log("Type my-tweets , spotify-this-song , movie-this , or do-what-it-say
 var userCommand = process.argv[2];
 var secondCommand = process.argv[3];
 
-	for(i=4; i<process.argv.length; i++){
-	    secondCommand += '+' + process.argv[i];
-	}
+for (i = 4; i < process.argv.length; i++) {
+	secondCommand += '+' + process.argv[i];
+}
 
-function theSwitch(){
-	
-	switch(userCommand){
+function theSwitch() {
+
+	switch (userCommand) {
 
 		case 'my-tweets':
-		fetchTweets();
-		break;
+			fetchTweets();
+			break;
 
 		case 'spotify-this-song':
-		spotifyMe();
-		break;
+			spotifyMe();
+			break;
 
 		case 'movie-this':
-		movieSearch();
-		break;
+			movieSearch();
+			break;
 
 		case 'do-what-it-says':
-		followRules();
-		break;
-		
+			followRules();
+			break;
+
 	}
 };
 
-function fetchTweets(){
+function fetchTweets() {
 	console.log("Here are your tweets");
-	
+
 	var client = new twitter({
 		consumer_key: keys.twitterKeys.consumer_key,
 		consumer_secret: keys.twitterKeys.consumer_secret,
@@ -50,97 +50,100 @@ function fetchTweets(){
 		access_token_secret: keys.twitterKeys.access_token_secret
 	});
 
-	
+
 	var parameters = {
 		screen_name: 'mr_thom_',
 		count: 20
 	};
 
-	client.get('statuses/user_timeline', parameters, function(error, tweets, response){
+	client.get('statuses/user_timeline', parameters, function (error, tweets, response) {
 		if (!error) {
-	        for (i=0; i<tweets.length; i++) {
-	            var returnedData = ('Number: ' + (i+1) + '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
-	            console.log(returnedData);
-	            console.log("-------------------------");
-	        }
-	    };
+			for (i = 0; i < tweets.length; i++) {
+				var returnedData = ('Number: ' + (i + 1) + '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
+				console.log(returnedData);
+				console.log("-------------------------");
+			}
+		};
 	});
 };
 
-function spotifyMe(){
+function spotifyMe() {
 	console.log("Search music");
 
 
 
 	var searchTrack;
-	if(secondCommand === undefined){
+	if (secondCommand === undefined) {
 		searchTrack = "The Sign";
-	}else{
+	} else {
 		searchTrack = secondCommand;
 	}
-	
-	spotify.search({type:'track', query:searchTrack}, function(err,data){
-	    if(err){
-	        console.log('Error occurred: ' + err);
-	        return;
-	    }else{
-	        
-	  		console.log("Artist: " + data.tracks.items[0].artists[0].name);
-	        console.log("Song: " + data.tracks.items[0].name);
-	        console.log("Album: " + data.tracks.items[0].album.name);
-	        console.log("Preview Here: " + data.tracks.items[0].preview_url);
-	    }
+
+	spotify.search({
+		type: 'track',
+		query: searchTrack
+	}, function (err, data) {
+		if (err) {
+			console.log('Error occurred: ' + err);
+			return;
+		} else {
+
+			console.log("Artist: " + data.tracks.items[0].artists[0].name);
+			console.log("Song: " + data.tracks.items[0].name);
+			console.log("Album: " + data.tracks.items[0].album.name);
+			console.log("Preview Here: " + data.tracks.items[0].preview_url);
+		}
 	});
 };
 
-function movieSearch(){
+function movieSearch() {
 	console.log("Search movies");
 
-	
+
 	var searchMovie;
-	if(secondCommand === undefined){
+	if (secondCommand === undefined) {
 		searchMovie = "Mr. Nobody";
-	}else{
+	} else {
 		searchMovie = secondCommand;
 	};
 
-	var url = 'http://www.omdbapi.com/?t=' + searchMovie +'&y=&plot=long&tomatoes=true&r=json';
-   	request(url, function(error, response, body){
-	    if(!error && response.statusCode == 200){
-	        console.log("Title: " + JSON.parse(body)["Title"]);
-	        console.log("Year: " + JSON.parse(body)["Year"]);
-	        console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
-	        console.log("Country: " + JSON.parse(body)["Country"]);
-	        console.log("Language: " + JSON.parse(body)["Language"]);
-	        console.log("Plot: " + JSON.parse(body)["Plot"]);
-	        console.log("Actors: " + JSON.parse(body)["Actors"]);
-	        console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
-	        console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
-	    }
-    });
+	var url = 'http://www.omdbapi.com/?t=' + searchMovie + '&y=&plot=long&tomatoes=true&r=json';
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log("Title: " + JSON.parse(body)["Title"]);
+			console.log("Year: " + JSON.parse(body)["Year"]);
+			console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
+			console.log("Country: " + JSON.parse(body)["Country"]);
+			console.log("Language: " + JSON.parse(body)["Language"]);
+			console.log("Plot: " + JSON.parse(body)["Plot"]);
+			console.log("Actors: " + JSON.parse(body)["Actors"]);
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
+			console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
+		}
+	});
 };
 
-function followRules(){
+function followRules() {
 	console.log("Looking at random.txt now");
-	fs.readFile("random.txt", "utf8", function(error, data) {
-	    if(error){
-     		console.log(error);
-     	}else{
+	fs.readFile("random.txt", "utf8", function (error, data) {
+		if (error) {
+			console.log(error);
+		} else {
 
-     	
-     	var dataArr = data.split(',');
-        userCommand = dataArr[0];
-        secondCommand = dataArr[1];
-        
-        for(i=2; i<dataArr.length; i++){
-            secondCommand = secondCommand + "+" + dataArr[i];
-        };
-        
-		theSwitch();
-		
-    	};
 
-    });
+			var dataArr = data.split(',');
+			userCommand = dataArr[0];
+			secondCommand = dataArr[1];
+
+			for (i = 2; i < dataArr.length; i++) {
+				secondCommand = secondCommand + "+" + dataArr[i];
+			};
+
+			theSwitch();
+
+		};
+
+	});
 
 };
 
